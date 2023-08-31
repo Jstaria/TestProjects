e8674426f5a1c34a2f5ea6954efce3d7a96ab91d
jws8554 <jws8554@rit.edu>
2023-08-31 13:59:43 -0400

    added inv class

---
+++ 2023 Projects/CSGOesc_Case_Opening/Inventory.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSGOesc_Case_Opening
+{
+    internal class Inventory
+    {
+    }
+}
