9c58cf650b9c206049f3540ad5b1dbb040a7c455
Jstaria <joseph-staria@hotmail.com>
2023-09-01 12:57:56 -0400

    some inventory stuff

--- 2023 Projects/CSGOesc_Case_Opening/Inventory.cs
+++ 2023 Projects/CSGOesc_Case_Opening/Inventory.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +10,35 @@ namespace CSGOesc_Case_Opening
 {
     internal class Inventory
     {
+        public int NumItems { get; private set; }
+
+        public Inventory() { }
+
+        public void Update(GameTime gameTime)
+        {
+
+        }
+
+        public void Draw(SpriteBatch sb)
+        {
+
+        }
+
+        /// <summary>
+        /// Adds items to a dictionary of dictionaries for each specific type of item
+        /// </summary>
+        public void AddItem(Item item)
+        {
+
+        }
+
+        /// <summary>
+        /// Remoes item with unique identifier
+        /// </summary>
+        /// <param name="item"></param>
+        public void RemoveItem(Item item)
+        {
+
+        }
     }
 }
