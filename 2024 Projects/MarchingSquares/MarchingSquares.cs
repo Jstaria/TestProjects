a81e23e43ec4162f925531b9957bc1594610f24f
jws8554 <joseph-staria@hotmail.com>
2024-01-26 15:35:38 -0500

    marching squares started

---
+++ 2024 Projects/MarchingSquares/MarchingSquares.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarchingSquares
+{
+    internal class MarchingSquares
+    {
+        private int width;
+        private int height;
+
+        private int[,] squareArray;
+
+        public MarchingSquares()
+        {
+
+        }
+    }
+}
---
+++ 2024 Projects/MarchingSquares/Program.cs
@@ -0,0 +1,3 @@
+﻿
+using var game = new MarchingSquares.Game1();
+game.Run();
