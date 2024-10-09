573b2427379a208d81dc094f26b57e94e0f222f1
jws8554 <jws8554@MSS2150-05>
2024-10-09 13:54:23 -0400

    Simple monogame extensions

---
+++ 2024 Projects/PostProcessing/Game1.cs
@@ -0,0 +1,96 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using MonoGame.Extended;
+using MonoGame.Extended.Graphics;
+
+namespace PostProcessing
+{
+    public class Game1 : Game
+    {
+        private GraphicsDeviceManager _graphics;
+        private SpriteBatch _spriteBatch;
+
+        private OrthographicCamera _camera;
+        private Sprite sprite;
+
+        private Effect postFX;
+
+        private RenderTarget2D _renderTarget;
+
+        public Game1()
+        {
+            _graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+            IsMouseVisible = true;
+
+            _graphics.PreferredBackBufferWidth = 1280;
+            _graphics.PreferredBackBufferHeight = 720;
+        }
+
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+
+            _renderTarget = new RenderTarget2D(GraphicsDevice, _graphics.PreferredBackBufferWidth, _graphics.PreferredBackBufferHeight);
+
+            sprite = new Sprite(Content.Load<Texture2D>("emoji_glasses"));
+            postFX = Content.Load<Effect>("postFX");
+
+            _camera = new OrthographicCamera(GraphicsDevice);
+
+            _camera.Position = new Vector2(0, 0);
+
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            _spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            // TODO: use this.Content to load your game content here
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            // TODO: Add your update logic here
+
+            KeyboardState state = Keyboard.GetState();
+            int speed = 10;
+
+            if (state.IsKeyDown(Keys.A)) _camera.Move(new Vector2( speed,  0));
+            if (state.IsKeyDown(Keys.W)) _camera.Move(new Vector2( 0,  speed));
+            if (state.IsKeyDown(Keys.D)) _camera.Move(new Vector2(-speed,  0));
+            if (state.IsKeyDown(Keys.S)) _camera.Move(new Vector2( 0, -speed));
+
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            // TODO: Add your drawing code here
+            GraphicsDevice.SetRenderTarget(_renderTarget);
+
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            _spriteBatch.Begin();
+
+            sprite.Draw(_spriteBatch, _camera.Position, 0, Vector2.One);
+
+            _spriteBatch.End();
+
+            GraphicsDevice.SetRenderTarget(null);
+
+            _spriteBatch.Begin(effect: postFX);
+
+            _spriteBatch.Draw(_renderTarget, new Vector2(0, 0), Color.White);
+
+            _spriteBatch.End();
+
+            base.Draw(gameTime);
+        }
+    }
+}
